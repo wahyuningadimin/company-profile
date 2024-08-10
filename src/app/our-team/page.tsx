30f36b4ad150a0862a08a52b79d26b30da9288f8
wahyuningadimin <wahyuningadimin@gmail.com>
2024-08-10 19:49:25 +0700

    edited4

--- src/app/our-team/page.tsx
+++ src/app/our-team/page.tsx
@@ -35,7 +35,7 @@ const OurTeamPage = async () => {
       <ul className='list-none p-0 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 gap-4 px-20 pt-5 pb-10'>
         {users.map((user, index) => (
           <li key={index} className='flex flex-col items-center bg-white border border-gray-300 rounded-lg p-4 shadow-md hover:shadow-lg transition-shadow duration-300'>
-            <Image src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} className='w-20 h-20 rounded-full mr-4'/>
+            <Image width={20} height={20} src={user.picture.large} alt={`${user.name.first} ${user.name.last}`} className='w-20 h-20 rounded-full mr-4'/>
             <div className='text-center'>
             <p className='text-xl font-semibold'>{user.name.first} {user.name.last}</p>
             <p className='text-gray-600'>{user.cell}</p>
